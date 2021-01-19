15b4fa32b7bdff8f043f1880287af226eb0db17f
Allma <allma9019@gmail.com>
2021-01-20 06:33:09 +0900

    quiz day2

--- 19_2_quiz.py
+++ 19_2_quiz.py
@@ -7,6 +7,7 @@ browser.get(url)
 
 # browser.find_element_by_xpath("//*[@id='queryInputHeader']").click()
 browser.find_element_by_xpath("//*[@id='queryInputHeader']").send_keys("마포래미안푸르지오") # 검색어에 입력
+browser.find_element_by_xpath("//*[@id='queryInputHeader']").send_keys("\n") # 엔터
+# browser.find_element_by_xpath("//*[@id='header']/div[1]/div/div[1]/div/fieldset/a[1]").click() # 검색 클릭
 
-
-browser.find_element_by_xpath("//*[@id='queryInputHeader']").send_keys("\n")
+# urlSe
