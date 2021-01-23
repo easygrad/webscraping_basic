c4af999ad0c1921b7cd1c1feeda2f181432ec992
Allma <allma9019@gmail.com>
2021-01-23 09:46:29 +0900

    quiz done!

--- 19_2_quiz.py
+++ 19_2_quiz.py
@@ -3,78 +3,54 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-url = "https://land.naver.com"
-browser = webdriver.Chrome()
-browser.get(url)
-browser.maximize_window()
+def landSearch(name):
 
-# browser.find_element_by_xpath("//*[@id='queryInputHeader']").click()
-browser.find_element_by_xpath("//*[@id='queryInputHeader']").send_keys("리센츠") # 검색어에 입력
-browser.find_element_by_xpath("//*[@id='queryInputHeader']").send_keys("\n") # 엔터
-# browser.find_element_by_xpath("//*[@id='header']/div[1]/div/div[1]/div/fieldset/a[1]").click() # 검색 클릭
+    url = "https://land.naver.com"
+    browser = webdriver.Chrome()
+    browser.get(url)
+    browser.maximize_window()
 
-
-
-interval = 2
-time.sleep(interval)
-
-prev_height = browser.execute_script("return document.body.scrollHeight")
-
-while True:
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    time.sleep(interval)
-    curr_height = browser.execute_script("return document.body.scrollHeight")
-    if curr_height == prev_height:
-        break
-    else:
-        curr_height = prev_height
-print("스크롤 완료")
-
-# def landSearch(name):
-
-#     url = "https://land.naver.com"
-#     browser = webdriver.Chrome()
-#     browser.get(url)
-#     browser.maximize_window()
-
-#     # browser.find_element_by_xpath("//*[@id='queryInputHeader']").click()
-#     browser.find_element_by_xpath("//*[@id='queryInputHeader']").send_keys(name) # 검색어에 입력
-#     browser.find_element_by_xpath("//*[@id='queryInputHeader']").send_keys("\n") # 엔터
-#     # browser.find_element_by_xpath("//*[@id='header']/div[1]/div/div[1]/div/fieldset/a[1]").click() # 검색 클릭
+    # browser.find_element_by_xpath("//*[@id='queryInputHeader']").click()
+    browser.find_element_by_xpath("//*[@id='queryInputHeader']").send_keys(name) # 검색어에 입력
+    browser.find_element_by_xpath("//*[@id='queryInputHeader']").send_keys("\n") # 엔터
+    # browser.find_element_by_xpath("//*[@id='header']/div[1]/div/div[1]/div/fieldset/a[1]").click() # 검색 클릭
     
-#     # interval = 2
-#     # time.sleep(interval)
+    time.sleep(1)
 
-#     # prev_height = browser.execute_script("return document.body.scrollHeight")
+    item_inners = browser.find_elements_by_class_name("item_inner ")
+    prev_len = len(item_inners)
 
-#     # while True:
-#     #     browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-#     #     time.sleep(interval)
-#     #     curr_height = browser.execute_script("return document.body.scrollHeight")
-#     #     if curr_height == prev_height:
-#     #         break
-#     #     else:
-#     #         curr_height = prev_height
-#     # print("스크롤 완료")
+    while True:
+        browser.execute_script("arguments[0].scrollIntoView(true)", item_inners[-1])
+        time.sleep(0.2)
+        curr_len = len(browser.find_elements_by_class_name("item_inner "))
+        print(curr_len, prev_len)
+        if curr_len == prev_len:
+            print("스크롤 완료")
+            print("-"*100)
+            break
+        else:
+            prev_len = curr_len
+            item_inners = browser.find_elements_by_class_name("item_inner ")
 
-#     soup = BeautifulSoup(browser.page_source, "lxml")
+    soup = BeautifulSoup(browser.page_source, "lxml")
 
-#     items = soup.find_all("div", attrs = {"class":"item_inner"})
-#     print("매물 개수: ", len(items))
-#     print("-"*100)
+    items = soup.find_all("div", attrs = {"class":"item_inner"})
+    print("매물 개수: ", len(items))
+    print("-"*100)
 
-#     for item in items:
-#         title = item.find("div", attrs = {"class":"item_title"}).get_text()
-#         price_type = item.find("span", attrs = {"class":"type"}).get_text()
-#         price = item.find("span", attrs = {"class":"price"}).get_text()
-#         spec = item.find("span", attrs = {"class":"spec"}).get_text()
-#         print(title)
-#         print(price_type, " : ", price)
-#         print(spec)
-#         print("-"*100)
-#         # price = item.find("")
+    for item in items:
+        title = item.find("div", attrs = {"class":"item_title"}).get_text()
+        price_type = item.find("span", attrs = {"class":"type"}).get_text()
+        price = item.find("span", attrs = {"class":"price"}).get_text()
+        spec = item.find("span", attrs = {"class":"spec"}).get_text()
+        print(title)
+        print(price_type, " : ", price)
+        print(spec)
+        print("-"*100)
+        # price = item.find("")
 
-#     browser.quit()
+    browser.quit()
 
-# name1 = "리센츠"
-# landSearch(name1)
+name1 = "리센츠"
+landSearch(name1)
